0807d1b9b6627e028dee1fe446bac64f033b76ab
Ridwan Bin Sarwar <38183049+ridwanbinsarwar@users.noreply.github.com>
2022-09-20 14:42:34 +0600

    Update Then.tsx

--- ConditionalRendering/Then.tsx
+++ ConditionalRendering/Then.tsx
@@ -2,8 +2,6 @@ import type { FC } from 'react';
 import { render } from '../render';
 
 /**
- * Must contain only a single child, which it renders as-is.
- * Should not be used outside of an `<If />` block.
- * @param props The props to pass down to the `<Then />` component
+ * Must contain only a single child
  */
 export const Then: FC = (props) => render(props);
