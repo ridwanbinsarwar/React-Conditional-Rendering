4d07cbb53d772ee93df150c8e3015f356415ff6c
Ridwan Bin Sarwar <38183049+ridwanbinsarwar@users.noreply.github.com>
2022-09-20 14:41:35 +0600

    Update If.tsx

--- ConditionalRendering/If.tsx
+++ ConditionalRendering/If.tsx
@@ -6,13 +6,9 @@ import { Then } from './Then';
  * If condition evaluates to true, renders the `<Then />` block will be rendered,
  * otherwise renders the `<Else />` block. Either block may be omitted.
  *
- * This component can contain any number of `<Then />` or `<Else />` blocks,
- * but only the first block of the right type (either Then or Else, depending on the condition) will be rendered.
- * @param __namedParameters The props to pass down to the `<IF />` component,
+ * only the first block of the right type (either Then or Else) will be rendered.
  */
-/**
- * Type for a value that can properly be parsed by `Boolean(...)`
- */
+
 export type BooleanLike = boolean | string | number | null | undefined;
 
 interface Props {
