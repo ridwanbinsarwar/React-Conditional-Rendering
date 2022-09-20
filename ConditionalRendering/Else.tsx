888334b3ae677c65257c86f6b4c113d6e2cb97fd
Ridwan Bin Sarwar <38183049+ridwanbinsarwar@users.noreply.github.com>
2022-09-20 14:42:15 +0600

    Update Else.tsx

--- ConditionalRendering/Else.tsx
+++ ConditionalRendering/Else.tsx
@@ -2,8 +2,6 @@ import type { FC } from 'react';
 import { render } from '../render';
 
 /**
- * Must only contain a single child, which it renders as-is.
- * Should not be used outside of an `<If />` block.
- * @param props The props to pass down to the `<Else />` component
+ * Must only contain a single child
  */
 export const Else: FC = (props) => render(props);
